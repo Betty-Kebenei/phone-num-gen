a2f76046591e8623080176f7150ff2475f41d4be
Betty-Kebenei <kebeneibetty13@gmail.com>
2019-07-30 11:39:49 +0300

    generate uniques phone numbers

--- src/components/generateNumbers/GenerateNumbers.jsx
+++ src/components/generateNumbers/GenerateNumbers.jsx
@@ -6,19 +6,26 @@ import FormForGeneration from './FormForGeneration';
 class GenerateNumbers extends Component {
   state = {
     quantity: 0,
-    phoneNumbers: []
+    phoneNumbers: [],
+    numberExists: false,
+  }
+
+  componentDidMount(){
+    if (localStorage.getItem("phonenumbers") === null) {
+      localStorage.setItem('phonenumbers', JSON.stringify([]))
+    }
   }
 
   handleOnChange = (event) => {
     this.setState({quantity: event.target.value});
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
     const { quantity } = this.state;
     this.setState({phoneNumbers: ''});
     let i = 0;
-    let existing;
+
     while(i<quantity){
       const arr = [0];
       for(let j=0; j<9; j += 1){
@@ -26,12 +33,53 @@ class GenerateNumbers extends Component {
         arr.push(digit);
       }
       const currentNumber = arr.join('');
-      existing = localStorage.getItem('phonenumbers');
-      existing = existing ? JSON.parse(existing) : [];
-      existing.push(currentNumber);
-      localStorage.setItem('phonenumbers', JSON.stringify(existing))
-      this.setState(prevState => ({phoneNumbers: [...prevState.phoneNumbers, currentNumber]}));
-      i += 1;
+
+      // check for uniqueness using binarySearch
+      const allNumbers = localStorage.getItem('phonenumbers');
+      if(allNumbers !== null){
+        const allNumbersArr = JSON.parse(allNumbers)
+        const numbers = allNumbersArr.sort();
+
+        // eslint-disable-next-line no-await-in-loop
+        await this.binarySearch(numbers, currentNumber);
+        const { numberExists } = this.state;
+        if(!numberExists){
+          this.addToLocalStorage(currentNumber);
+          i += 1;
+        }
+      }
+      else {
+        this.addToLocalStorage(currentNumber);
+        i += 1;
+      }
+    }
+  }
+
+  addToLocalStorage = (currentNumber) => {
+    let existing;
+    existing = localStorage.getItem('phonenumbers');
+    existing = existing ? JSON.parse(existing) : [];
+    existing.push(currentNumber);
+    localStorage.setItem('phonenumbers', JSON.stringify(existing))
+    this.setState(prevState => ({phoneNumbers: [...prevState.phoneNumbers, currentNumber]}));
+  }
+
+  binarySearch = (arr, key) => {
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+
+    while(startIndex <= endIndex) {
+      const mid = Math.floor((startIndex + endIndex) / 2);
+
+      if(key === arr[mid]) {
+        this.setState({numberExists: true})
+      }
+      else if(key > arr[mid]) {
+        startIndex = mid + 1;
+      }
+      else {
+        endIndex = mid - 1;
+      }
     }
   }
 
--- src/components/generateNumbers/__tests__/GenerateNumbers.test.js
+++ src/components/generateNumbers/__tests__/GenerateNumbers.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import {configure, mount } from 'enzyme';
-
 import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon'
 
 import GenerateNumber from '../GenerateNumbers';
+
 
 configure({ adapter: new Adapter() })
 
@@ -22,6 +23,16 @@ describe('GenerateNumber', () => {
       wrapper.find('[name="quantity"]').simulate('change', {target: {name: 'quantity', value: 10}});
       wrapper.find('[type="submit"]').simulate('click');
       expect(wrapper.state().quantity).toEqual(10);
+    })
+  })
+
+  describe('addToLocalStorage', () => {
+    test('adding to localstorage', () => {
+      const wrapper = mount(<GenerateNumber />);
+      const renderedComponent = wrapper.instance();
+      sinon.spy(renderedComponent, 'addToLocalStorage');
+      renderedComponent.addToLocalStorage('021735551');
+      expect(wrapper.state('phoneNumbers').length).toEqual(1);
     })
   })
 
